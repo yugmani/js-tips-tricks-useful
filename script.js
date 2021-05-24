21d52e86c06e03ebb98cebcaf11226188fcecbb3
yugmani <yugmani@yahoo.com>
2021-05-23 23:45:06 -0700

    function to execute event handler only once

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ const lastFourDigits = creditCard.slice(-4); //get last 4 digits;
 
 const maskedNumber = lastFourDigits.padStart(creditCard.length, '*');
 
-console.log(lastFourDigits); //Expected 4321
-console.log(maskedNumber); // Expected ************4321
+// console.log(lastFourDigits); //Expected 4321
+// console.log(maskedNumber); // Expected ************4321
 
 /* ***************************************************** */
 
 // Execute an Event handler only once
+
+// By passing { once: true } as the third argument to the addEventListener method, the event handler function will be executed only once.
+
+document.getElementById('btn').addEventListener(
+  'click',
+  function() {
+    console.log('Button clicked!');
+  },
+  { once: true } // to execute only once.
+);
