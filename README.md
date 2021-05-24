132ff86caf46a224b9953319cfe231c23e4dfeff
yugmani <yugmani@yahoo.com>
2021-05-23 23:36:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-tips-tricks-useful
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tips-tricks-useful)
---
+++ script.js
@@ -0,0 +1,11 @@
+//Quick way to mask numbers using slce and ES8 padStart Method
+
+const creditCard = '4111111111114321'; //16 digit credit card numbers
+const lastFourDigits = creditCard.slice(-4); //get last 4 digits;
+
+//prepend * to lastFourDigits to make length equal to creditCard number length
+
+const maskedNumber = lastFourDigits.padStart(creditCard.length, '*');
+
+console.log(lastFourDigits); //Expected 4321
+console.log(maskedNumber); // Expected ************4321
